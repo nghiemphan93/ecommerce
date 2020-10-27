f6f510d530bc785f4c2b26bf6f3ed1ce24faac7d
Phan <nghiemphan93@gmail.com>
2020-10-28 00:05:36 +0100

    added exception handling and swagger

--- Core/Specifications/ISpecification.cs
+++ Core/Specifications/ISpecification.cs
@@ -1,7 +1,12 @@
-﻿namespace Core.Specifications
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace Core.Specifications
 {
-  public interface ÍSpecification
+  public interface ISpecification<T>
   {
-    
+    Expression<Func<T, bool>> Criteria { get; }
+    List<Expression<Func<T, object>>> Includes { get; }
   }
 }
